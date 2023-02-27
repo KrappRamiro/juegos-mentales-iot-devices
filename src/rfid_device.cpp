f0d07b64d2c9aeefcb152b98c08d6ad51e324a04
Krapp Ramiro <krappramiro@disroot.org>
2023-02-26 21:30:53 -0300

    add untested version of rfid-device generic file

--- src/rfid_device.cpp
+++ src/rfid_device.cpp
@@ -1,8 +1,26 @@
 #include "utils/iot_utils.hpp"
 #include "utils/multiple_rfid_utils.hpp"
 
-String lastPub[NUMBER_OF_READERS]; // should be in the mixin
+String lastPub[NUMBER_OF_READERS];
 bool should_publish;
+
+void report_rfid_to_broker()
+{
+	StaticJsonDocument<256> doc;
+	char jsonBuffer[256];
+	for (int i = 0; i < NUMBER_OF_READERS; i++) {
+		lastPub[i] = getUIDFromReadingStorage(i);
+		Serial.printf("Adding to the doc: \"rfid_%s\" : ", String(i));
+		Serial.println(lastPub[i]);
+		doc[String("rfid_" + String(i))] = lastPub[i];
+	}
+	serializeJsonPretty(doc, jsonBuffer);
+	Serial.println("Reporting the following to the mqtt broker:");
+	Serial.println(jsonBuffer);
+	String result = (mqttc.publish(READING_TOPIC, jsonBuffer)) ? "success publishing" : "failed publishing";
+	Serial.println(result);
+}
+
 void messageHandler(char* topic, byte* payload, unsigned int length)
 {
 	if (strcmp(topic, RESET_TOPIC) == 0) {
@@ -10,6 +28,7 @@ void messageHandler(char* topic, byte* payload, unsigned int length)
 			lastPub[i] = "00 00 00 00";
 		}
 		Serial.println("Cleaning lastPub");
+		// debug("Cleaning lastPub")
 		clearReadingStorage();
 	}
 }
@@ -20,14 +39,34 @@ void setup()
 		; // Do nothing until serial connection is opened
 	connect_mqtt_broker(THINGNAME);
 	SPI.begin(); // Init SPI bus
-	mfrc522[0].PCD_SetAntennaGain(mfrc522[0].RxGain_max);
-	mfrc522[1].PCD_SetAntennaGain(mfrc522[1].RxGain_max);
-	mfrc522[2].PCD_SetAntennaGain(mfrc522[2].RxGain_max);
-	mfrc522[3].PCD_SetAntennaGain(mfrc522[3].RxGain_max);
+	for (int i = 0; i < NUMBER_OF_READERS; i++) {
+		mfrc522[i].PCD_SetAntennaGain(mfrc522[i].RxGain_max);
+	}
 	printRFIDVersions();
 	pinMode(RST_PIN, OUTPUT);
 	digitalWrite(RST_PIN, LOW); // mfrc522 readers hard power down.
 	mqttc.subscribe(RESET_TOPIC);
 	mqttc.setCallback(messageHandler);
 }
-void loop() { }
+void loop()
+{
+	if (!mqttc.connected()) {
+		reconnect();
+	}
+	printMultipleRFID();
+	if (newRFIDAppeared) {
+		should_publish = false;
+		for (int i = 0; i < NUMBER_OF_READERS; i++) {
+			if (!(lastPub[i].equals(getUIDFromReadingStorage(i)))) {
+				should_publish = true;
+			}
+		}
+		if (should_publish) {
+			report_rfid_to_broker();
+		} else {
+			Serial.println("Not publishing because the state is the same as before");
+		}
+		newRFIDAppeared = false;
+	}
+	local_delay(200);
+}
